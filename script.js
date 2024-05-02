01cb1c70928d8adfb648e4444b2df272ce23eebf
bini-x <erblinhiseni@gmail.com>
2024-05-02 21:20:47 +0200

    Added the JS file + new feature(dark mode)

---
+++ script.js
@@ -0,0 +1,8 @@
+let darkModeBtn = document.querySelector('.darkModeBtn');
+let body = document.querySelector('body')
+
+darkModeBtn.addEventListener('click', () => {
+	body.classList.toggle('darkMode');
+})
+
+
