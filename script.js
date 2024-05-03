906cf8a4a358f942a29e2815d4fb4938025cd7aa
bini-x <erblinhiseni@gmail.com>
2024-05-03 21:56:00 +0200

    added responsivity

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let body = document.querySelector('body')
 
 darkModeBtn.addEventListener('click', () => {
 	body.classList.toggle('darkMode');
+	if (darkModeBtn.innerText === "Dark Mode") {
+		darkModeBtn.innerText = "Light Mode";
+	} else {
+		darkModeBtn.innerText = 'Dark Mode';
+	}
 })
+
 
 
